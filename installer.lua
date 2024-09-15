a218d08b0864527c0343dfe20ab8582ab69c0ec4
HumaneWolf <humanewolf@gmail.com>
2024-09-15 16:36:25 +0200

    Storage counter

--- installer.lua
+++ installer.lua
@@ -3,12 +3,12 @@ local systemName = ({ ... })[1]
 local ghBaseUrl = "https://raw.githubusercontent.com/HumaneWolf/cct-scripts/main/"
 
 local systemDefs = {
-    ["metrics:host"] = {
-        ["server"] = "monitoring/metricshost.lua",
-    },
     ["canal:digger"] = {
-        ["canal"] = "canals/digger.lua",
+        ["canal.lua"] = "canals/digger.lua",
         ["turtlehelper.lua"] = "helpers/turtle.lua",
+    },
+    ["storage"] = {
+        ["storage.lua"] = "storage/storage.lua",
     },
 }
 
---
+++ storage/storage.lua
@@ -0,0 +1,74 @@
+-- Options
+local HEIGHT = 3
+
+
+-- Functions
+local function countChest(direction, index)
+    local inv = peripheral.wrap(direction)
+    if inv == nil or inv.list == nil then
+        print('No inventory found to the '..direction)
+        return false, index
+    end
+
+    local items = inv.list()
+    for slot, item in pairs(items) do
+        print(slot..' - '..item)
+        if index[item.name] ~= nil then
+            index[item.name] = 0
+        end
+        index[item.name] = index[item.name] + item.count
+    end
+end
+
+
+-- Main
+local currentHeight = 1
+local movement = 1
+
+local currentIndex = {}
+local hasChestRight, hasChestLeft
+
+--while true do -- Main loop
+while true do -- Move and check loop
+    -- Check
+    hasChestRight, currentIndex = countChest('right', currentIndex)
+    hasChestLeft, currentIndex = countChest('left', currentIndex)
+
+    -- Find direction and move on
+    if hasChestLeft == false and hasChestRight == false then
+        print('Loop done')
+        turtle.turnLeft()
+        turtle.turnLeft()
+        turtle.forward()
+        break
+    elseif movement == 1 and (currentHeight == HEIGHT or turtle.detectUp()) then -- If done with column
+        if turtle.detect() then
+            print('Cannot move forward.')
+            os.exit(100, true)
+        end
+        turtle.forward()
+        movement = -1
+    elseif movement == -1 and (currentHeight == 1 or turtle.detectDown()) then
+        if turtle.detect() then
+            print('Cannot move forward.')
+            os.exit(100, true)
+        end
+        turtle.forward()
+        movement = 1
+    elseif movement == 1 then
+        if turtle.detectUp() then
+            print('Cannot move up.')
+            os.exit(100, true)
+        end
+        turtle.up()
+        currentHeight = currentHeight + 1
+    elseif movement == -1 then
+        if turtle.detectDown() then
+            print('Cannot move down.')
+            os.exit(100, true)
+        end
+        turtle.down()
+        currentHeight = currentHeight - 1
+    end
+end
+--end
