6f403922fe41f86cd1c11d674d982e6157e25ece
HumaneWolf <humanewolf@gmail.com>
2024-09-15 16:59:00 +0200

    fix

--- storage/storage.lua
+++ storage/storage.lua
@@ -14,7 +14,7 @@ local function countChest(direction, index)
     local items = inv.list()
     for slot, item in pairs(items) do
         print(slot..' - '..item.name..' ('..item.count..')')
-        if index[item.name] ~= nil then
+        if index[item.name] == nil then
             index[item.name] = 0
         end
         index[item.name] = index[item.name] + item.count
