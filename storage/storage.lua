4d9a6d70d0909f596950c3a010af9797b70b2638
HumaneWolf <humanewolf@gmail.com>
2024-09-15 17:08:32 +0200

    Loop it

--- storage/storage.lua
+++ storage/storage.lua
@@ -43,53 +43,54 @@ local movement = 1
 local currentIndex = {}
 local hasChestRight, hasChestLeft
 
---while true do -- Main loop
-while true do -- Move and check loop
-    -- Check
-    hasChestRight, currentIndex = countChest('right', currentIndex)
-    hasChestLeft, currentIndex = countChest('left', currentIndex)
+while true do -- Main loop
+    while true do -- Move and check loop
+        -- Check
+        hasChestRight, currentIndex = countChest('right', currentIndex)
+        hasChestLeft, currentIndex = countChest('left', currentIndex)
 
-    -- Find direction and move on
-    if hasChestLeft == false and hasChestRight == false then
-        print('Loop done')
-        turtle.turnLeft()
-        turtle.turnLeft()
-        executeOrWait(turtle.forward)
-        break
-    elseif movement == 1 and (currentHeight == HEIGHT or turtle.detectUp()) then -- If done with column
-        if turtle.detect() then
-            print('Cannot move forward.')
-            os.exit(100, true)
+        -- Find direction and move on
+        if hasChestLeft == false and hasChestRight == false then
+            print('Loop done. Will pause for a bit.')
+            turtle.turnLeft()
+            turtle.turnLeft()
+            os.sleep(60)
+            executeOrWait(turtle.forward)
+            break
+        elseif movement == 1 and (currentHeight == HEIGHT or turtle.detectUp()) then -- If done with column
+            if turtle.detect() then
+                print('Cannot move forward.')
+                os.exit(100, true)
+            end
+            executeOrWait(turtle.forward)
+            movement = -1
+        elseif movement == -1 and (currentHeight == 1 or turtle.detectDown()) then
+            if turtle.detect() then
+                print('Cannot move forward.')
+                os.exit(100, true)
+            end
+            executeOrWait(turtle.forward)
+            movement = 1
+        elseif movement == 1 then
+            if turtle.detectUp() then
+                print('Cannot move up.')
+                os.exit(100, true)
+            end
+            executeOrWait(turtle.up)
+            currentHeight = currentHeight + 1
+        elseif movement == -1 then
+            if turtle.detectDown() then
+                print('Cannot move down.')
+                os.exit(100, true)
+            end
+            executeOrWait(turtle.down)
+            currentHeight = currentHeight - 1
         end
-        executeOrWait(turtle.forward)
-        movement = -1
-    elseif movement == -1 and (currentHeight == 1 or turtle.detectDown()) then
-        if turtle.detect() then
-            print('Cannot move forward.')
-            os.exit(100, true)
-        end
-        executeOrWait(turtle.forward)
-        movement = 1
-    elseif movement == 1 then
-        if turtle.detectUp() then
-            print('Cannot move up.')
-            os.exit(100, true)
-        end
-        executeOrWait(turtle.up)
-        currentHeight = currentHeight + 1
-    elseif movement == -1 then
-        if turtle.detectDown() then
-            print('Cannot move down.')
-            os.exit(100, true)
-        end
-        executeOrWait(turtle.down)
-        currentHeight = currentHeight - 1
     end
-end
 
-for item, count in pairs(currentIndex) do
-    print(item..': '..count)
-end
+    for item, count in pairs(currentIndex) do
+        print(item..': '..count)
+    end
 
-currentIndex = {}
---end
+    currentIndex = {}
+end
